a269b867bffc2d50adbfd986a09e04adf609900d
Brock <brock.m.tillotson@gmail.com>
2017-09-23 14:17:08 -0700

    set a hello world page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #request comes in from the route to the hello action 'application#hello'
+  #this renders html text
+  def hello
+    render html: "hello, world!"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #sets the root page of the application to go to the application controller
+  root 'application#hello'
+
 end
