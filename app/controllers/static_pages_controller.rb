43d2462fcfa2fafabbf5eca3aba529ff16e4df6b
Brock <brock.m.tillotson@gmail.com>
2017-09-23 16:16:21 -0700

    saving work, up to listing 3.36

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+
+  #even if this is empty, it will still serve the view
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+
+
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  #maps request for URL /static_pages/home to the home action in the controller
+  get 'static_pages/home'
+  #same thing for other pages
+  get 'static_pages/help'
+  get 'static_pages/about'
 
   #sets the root page of the application to go to the application controller
   root 'application#hello'
