9dba812c518d126a0a2607bdf629f353bd0e17d5
Brock <brock.m.tillotson@gmail.com>
2017-09-25 10:16:14 -0700

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  #request comes in from the route to the hello action 'application#hello'
-  #this renders html text
-  def hello
-    render html: "hello, world!"
-  end
+  
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-
+  #sets the root page of the application to go to the static_pages controller and do the home action
+  root 'static_pages#home'
   #maps request for URL /static_pages/home to the home action in the controller
   get 'static_pages/home'
   #same thing for other pages
   get 'static_pages/help'
   get 'static_pages/about'
 
-  #sets the root page of the application to go to the application controller
-  root 'application#hello'
+
 
 end
